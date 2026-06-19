96f87b018a15fb50b80b74980bc0b276e69bb2d6
techyusman <usmanalee691@gmail.com>
2026-06-19 17:26:35 +0500

    Add and update Python basic script

---
+++ python_basic.py
@@ -0,0 +1,19 @@
+# Python has Dynamic Variable capability it will recoginze the data type automatically at run time.
+name = "M Usman"
+age = 21
+student = True
+
+# For Print we use these statements 
+
+print((name))
+print((age))
+
+# to check the type of variable
+print(type(student))
+
+# Changing its type 
+new_age = "21"
+age = int(new_age)
+print(type(age))
+
+
