2f2207d77e5f4bdad78432d96823402460c55921
techyusman <usmanalee691@gmail.com>
2026-06-25 15:25:20 +0500

    Draw graphs

--- Week 2/Seaborn.py
+++ Week 2/Seaborn.py
@@ -22,7 +22,32 @@ print(Data['island'].value_counts())
 # darkgird
 # ticks
 
-sns.set_style("whitegrid")
+#sns.set_style("whitegrid")
 #create the scatterplot based on flipper length and bill length based on island
-sns.scatterplot(data = Data, x = "flipper_length_mm", y ="bill_length_mm", hue="island")
-#plt.show()
+#sns.scatterplot(data = Data, x = "flipper_length_mm", y ="bill_length_mm", hue="island")
+
+# Creating Stripplot
+#sns.stripplot(data = Data, x = "species" , y = "bill_length_mm", hue = "island", dodge=True)
+
+
+# Creating swarmplot
+#sns.swarmplot(data = Data, x = "species" , y = "bill_length_mm", hue = "island")
+
+# Visualizing one features using histogram and distigustion based on sex
+
+#sns.histplot(Data, x = "body_mass_g", hue = "sex", multiple="stack")
+
+# line plot 
+#sns.lineplot(Data, x = "body_mass_g" , y = "flipper_length_mm", hue = "sex")
+
+
+# Joint plot combining two different plot, historgram and scatter plot
+#sns.jointplot(Data, x="body_mass_g", y= "flipper_length_mm", hue="sex")
+
+# Bar Plot
+#sns.barplot(Data, x="species", y = "bill_length_mm", hue = "sex",)
+
+#box plot, it help us to identify the outlier in the data
+
+sns.boxplot(Data, x="species", y = "body_mass_g", hue = "sex")
+plt.show()
